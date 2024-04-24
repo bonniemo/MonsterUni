003bbf43da58a6cb8930b870367a9851191eb594
Bonnie <moller.bon@gmail.com>
2024-04-24 14:59:41 +0200

    added pick avatar to add monster

--- src/Components/MonsterBox/FormInput.tsx
+++ src/Components/MonsterBox/FormInput.tsx
@@ -1,14 +1,21 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useFormInput } from "../../Hooks/useFormInput";
 import { GlobalContext } from "../../State/GlobalStateContext";
 import { v4 as uuidv4 } from "uuid";
+import avatar1 from '../../Assets/MonsterAvatars/ice-totoro.png';
+import avatar2 from '../../Assets/MonsterAvatars/fire-mane.png'
 
 const FormInput = () => {
-  const { state, dispatch } = useContext(GlobalContext);
+  const { dispatch } = useContext(GlobalContext);
 
   const firstName = useFormInput("");
   const lastName = useFormInput("");
   const description = useFormInput("");
+  const [avatar, setAvatar] = useState("");
+
+  const handleAvatarClick = (avatar:string) => {
+    setAvatar(avatar)
+  }
 
   const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -20,13 +27,17 @@ const FormInput = () => {
         first_name: firstName.value,
         last_name: lastName.value,
         description: description.value,
+        avatar: avatar,
       },
-    });
-    console.log(state.monsters);
+    });    
   };
 
   return (
+    <>
+    <img src={avatar1} alt="" onClick={() => handleAvatarClick(avatar1)} />
+    <img src={avatar2} alt="" onClick={() => handleAvatarClick(avatar2)} />
     <form onSubmit={handleSubmit}>
+       
       <>
         <label htmlFor="firstName">First Name:</label>
         <input type="text" {...firstName} id="firstName" />
@@ -40,7 +51,8 @@ const FormInput = () => {
         <input type="text" {...description} id="description" />
       </>
       <button type="submit">Add Monster</button>
-    </form>
+    </form>    
+    </>
   );
 };
 
