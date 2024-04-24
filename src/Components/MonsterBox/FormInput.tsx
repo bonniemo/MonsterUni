d71ef3cf359ddb09c8f0c1a6435d19a879358fb9
Bonnie <moller.bon@gmail.com>
2024-04-24 15:33:58 +0200

    improved avatar render on forminput, start of styling

--- src/Components/MonsterBox/FormInput.tsx
+++ src/Components/MonsterBox/FormInput.tsx
@@ -2,8 +2,11 @@ import { useContext, useState } from "react";
 import { useFormInput } from "../../Hooks/useFormInput";
 import { GlobalContext } from "../../State/GlobalStateContext";
 import { v4 as uuidv4 } from "uuid";
-import avatar1 from '../../Assets/MonsterAvatars/ice-totoro.png';
-import avatar2 from '../../Assets/MonsterAvatars/fire-mane.png'
+import "./FormInput.scss";
+import avatar1 from "../../Assets/MonsterAvatars/ice-totoro.png";
+import avatar2 from "../../Assets/MonsterAvatars/fire-mane.png";
+import avatar3 from "../../Assets/MonsterAvatars/many-eyes.png";
+import avatar4 from "../../Assets/MonsterAvatars/rainbow-wings.png";
 
 const FormInput = () => {
   const { dispatch } = useContext(GlobalContext);
@@ -11,13 +14,13 @@ const FormInput = () => {
   const firstName = useFormInput("");
   const lastName = useFormInput("");
   const description = useFormInput("");
-  const [avatar, setAvatar] = useState("");
+  const [chosenAvatar, setChosenAvatar] = useState("");
 
-  const handleAvatarClick = (avatar:string) => {
-    setAvatar(avatar)
-  }
+  const handleAvatarClick = (avatar: string) => {
+    setChosenAvatar(avatar);
+  };
 
-  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     dispatch({
@@ -27,31 +30,42 @@ const FormInput = () => {
         first_name: firstName.value,
         last_name: lastName.value,
         description: description.value,
-        avatar: avatar,
+        avatar: chosenAvatar,
       },
-    });    
+    });
   };
+
+  const avatarArr = [avatar1, avatar2, avatar3, avatar4];
 
   return (
     <>
-    <img src={avatar1} alt="" onClick={() => handleAvatarClick(avatar1)} />
-    <img src={avatar2} alt="" onClick={() => handleAvatarClick(avatar2)} />
-    <form onSubmit={handleSubmit}>
-       
-      <>
-        <label htmlFor="firstName">First Name:</label>
-        <input type="text" {...firstName} id="firstName" />
-      </>
-      <>
-        <label htmlFor="lastName">Last Name:</label>
-        <input type="text" {...lastName} id="lastName" />
-      </>
-      <>
-        <label htmlFor="description">Description:</label>
-        <input type="text" {...description} id="description" />
-      </>
-      <button type="submit">Add Monster</button>
-    </form>    
+      <section className="pickAvatar">
+        {avatarArr.map((avatar, index) => (
+          <img
+            key={index}
+            src={avatar}
+            alt={`Avatar ${index + 1}`}
+            onClick={() => handleAvatarClick(avatar)}
+            className={chosenAvatar === avatar ? "chosen" : ""}
+          />
+        ))}
+      </section>
+
+      <form onSubmit={handleSubmit}>
+        <>
+          <label htmlFor="firstName">First Name:</label>
+          <input type="text" {...firstName} id="firstName" />
+        </>
+        <>
+          <label htmlFor="lastName">Last Name:</label>
+          <input type="text" {...lastName} id="lastName" />
+        </>
+        <>
+          <label htmlFor="description">Description:</label>
+          <input type="text" {...description} id="description" />
+        </>
+        <button type="submit">Add Monster</button>
+      </form>
     </>
   );
 };
